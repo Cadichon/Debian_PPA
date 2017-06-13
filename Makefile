27b6c83b54537d0da3977d0e330eeaf95e6bea00
Cadichon <leo.labruyere@epitech.eu>
2017-06-13 16:54:16 +0200

    first commit

---
+++ Makefile
@@ -0,0 +1,41 @@
+##
+## Makefile for add-apt-repository in /home/cadichon/Epitech/Debian_PPA
+## 
+## Made by Cadichon
+## Login   <leo.labruyere@epitech.eu>
+## 
+## Started on  Tue Jun 13 16:45:37 2017 Cadichon
+## Last update Tue Jun 13 16:52:08 2017 Cadichon
+##
+
+SRC	=	get_ppa_key.c
+
+OBJ	=	$(SRC:.c=.o)
+
+NAME	=	get_ppa_key
+
+PROJECT =	add-apt-repository
+
+CFLAGS	=	-W -Wall -Werror
+
+all:		$(NAME)
+
+$(NAME):	$(OBJ)
+		$(CC) $(OBJ) -o $(NAME)
+
+clean:
+		rm -rf $(OBJ)
+
+fclean:		clean
+		rm -rf $(NAME)
+
+re:		fclean all
+
+dbg:		CFLAGS += -g
+dbg:		re
+
+install:
+		cp $(NAME) /usr/bin/$(NAME)
+		cp $(PROJECT) /usr/bin/$(NAME)
+
+.PHONY:		all clean fclean dbg
---
+++ get_ppa_key.c
@@ -0,0 +1,68 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define LAST_CHAR(str) str[strlen(str) - 1]
+
+int		main(int argc, char *argv[])
+{
+  char		**key;
+  char		*str;
+  char		*ptr;
+  size_t	size;
+  char		*cmd;
+  FILE		*fd;
+  int		i;
+
+  if (argc != 2)
+    {
+      dprintf(2, "Utility for add-apt-repository\n");
+      dprintf(2, "Please specify apt update error log\n");
+      return (1);
+    }
+  if ((fd = fopen(argv[1], "r")) == NULL)
+    {
+      dprintf(2, "Can't open %s\n", argv[1]);
+      return (1);
+    }
+  str = NULL;
+  size = 0;
+  i = 0;
+  key = calloc(32, sizeof(char *));
+  while (getline(&str, &size, fd) != -1)
+    {
+      if ((ptr = strstr(str, "NO_PUBKEY")) != NULL)
+	{
+	  key[i] = strdup(ptr + 10);
+	  LAST_CHAR(key[i]) = '\0';
+	  i++;
+	}
+    }
+  free(str);
+  key[i] = NULL;
+  if (i == 0)
+    {
+      free(key);
+      dprintf(2, "No public key found, exitting...\n");
+      return (1);
+    }
+  i = 0 ;
+  dprintf(1, "%s\n", "Public key to add :");
+  while (key[i])
+    {
+      dprintf(1, "- %s\n", key[i]);
+      i++;
+    }
+  i = 0;
+  while (key[i])
+    {
+      asprintf(&cmd, "%s %s", "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys", key[i]);
+      free(key[i]);
+      system(cmd);
+      free(cmd);
+      i++;
+    }
+  free(key);
+  fclose(fd);
+}
